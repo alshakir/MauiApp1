a9ec8e18bffb60697695302914df9986280c8ce5
alsha <alsha@DESKTOP-0ACI66U>
2022-06-17 12:05:28 +0300

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+		
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private async void popupBtn_Clicked(object sender, EventArgs e)
+	{
+		count++;
+        CounterBtn.Text = $"Clicked {count} times";
+		CounterBtn.WidthRequest = 500;
+		input1.Focus();
+		CounterBtn.Focus();
+		img1.HeightRequest = 100;
+		img1.WidthRequest = 100;
+
+        //await DisplayAlert("This is the best ALERT", "How cool is MAUI technology!!", "OKKK!!");
+        //string s = await DisplayPromptAsync("Title", "messgaaaaaaa");
+
+        //string s2 = await DisplayActionSheet("Your hobbies", "cancel!!!", "destroy", "food", "water", "chess", "movie");
+    }
+}
+
